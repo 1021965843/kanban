ebdae4ae292e52f9cad7d91382dd6cc82dadd1ff
1021965843 <1021965843@qq.com>
2021-06-13 02:33:41 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import "./plugins/filters"
+// import "./plugins/filters"
+//时间过滤器
+Vue.filter('dateFormat', function (timeStr) {
+  const date = new Date(timeStr)
+
+  let Y = date.getFullYear()
+  let M = date.getMonth() + 1 + ""
+  let D = date.getDate() + ""
+  let weekDay = "日一二三四五六".charAt(date.getDay())
+  let dateH = (date.getHours() + "").padStart(2, "0");
+  let dateM = (date.getMinutes() + "").padStart(2, "0");
+
+  return ` ${Y}/${M}/${D}  星期${weekDay} ${dateH}:${dateM}`
+})
 
 Vue.config.productionTip = false
 
